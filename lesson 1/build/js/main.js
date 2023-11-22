396e9c4a63ee9310b5d8b4cc1051bd7b0f50fa54
Dhanraj Singh <sdhanraj300@gmail.com>
2023-11-22 23:55:34 +0530

    lesson

--- lesson 1/build/js/main.js
+++ lesson 1/build/js/main.js
@@ -1,17 +1,6 @@
 "use strict";
-let stringArr = ["one", "two", "three"];
-let guitars = ["strat", "tele", 512];
-let numbers = [1, 2, 3, 4, 5];
-stringArr.push("four");
-let myObj = {};
-myObj = [1, 2, 3, 4, 5];
-const obj = {
-    prop1: "value1",
-    prop2: "value2",
-    prop3: 42,
-};
-let evh = {
-    name: "Eddie Van Halen",
-    active: true,
-    albums: ["Van Halen I", "Van Halen II", 1984],
-};
+// let stringArr: string[] = ["one", "two", "three"];
+// let guitars = ["strat", "tele", 512];
+// let numbers: number[] = [1, 2, 3, 4, 5];
+let userName;
+userName = "Jane";
--- lesson 1/src/main.ts
+++ lesson 1/src/main.ts
@@ -1,27 +1,54 @@
-let stringArr: string[] = ["one", "two", "three"];
-let guitars = ["strat", "tele", 512];
-let numbers: number[] = [1, 2, 3, 4, 5];
+// let stringArr: string[] = ["one", "two", "three"];
+// let guitars = ["strat", "tele", 512];
+// let numbers: number[] = [1, 2, 3, 4, 5];
 
-stringArr.push("four");
+// stringArr.push("four");
 
-let myObj: object = {};
+// let myObj: object = {};
 
-myObj = [1, 2, 3, 4, 5];
+// myObj = [1, 2, 3, 4, 5];
 
-const obj = {
-  prop1: "value1",
-  prop2: "value2",
-  prop3: 42,
-};
+// const obj = {
+//   prop1: "value1",
+//   prop2: "value2",
+//   prop3: 42,
+// };
+
+// interface Guitarist {
+//   name: string;
+//   active?: boolean;
+//   albums: (string | number)[];
+// }
+
+// let evh: Guitarist = {
+//   name: "Eddie Van Halen",
+//   active: true,
+//   albums: ["Van Halen I", "Van Halen II", 1984],
+// };
+
+// const logGuitarist = (guitarist: Guitarist) => {
+//   return `${guitarist.name} is ${guitarist.active ? "active" : "not active"}`;
+// };
+// console.log(logGuitarist(evh));
+
+// enum Grade {
+//   U,
+//   D,
+//   C,
+//   B,
+//   A,
+// }
+// console.log(Grade.C)
+
+//Type Aliases
+type stringOrNumber = string | number;
+type stringOrNumberArr = (string | number)[];
 
 type Guitarist = {
-  name: string;
-  active: boolean;
-  albums: (string | number)[];
+  name?: string;
+  guitars: stringOrNumber;
+  albums: stringOrNumberArr;
 };
 
-let evh: Guitarist = {
-  name: "Eddie Van Halen",
-  active: true,
-  albums: ["Van Halen I", "Van Halen II", 1984],
-};
+let userName: "John" | "Jane" | "Joe";
+userName = "Jane";
